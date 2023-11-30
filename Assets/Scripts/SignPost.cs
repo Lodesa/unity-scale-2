8656fceac32d5017591156b668b2d66fa04383bf
cjulian <4729253+cjulian@users.noreply.github.com>
2023-11-30 17:24:53 -0500

    skeleton counter

--- Assets/Scripts/SignPost.cs
+++ Assets/Scripts/SignPost.cs
@@ -5,15 +5,31 @@ using TMPro;
 using UnityEngine;
 
 public class SignPost : MonoBehaviour {
+  public GameObject skeletonCounter;
+  private SkeletonCounter _skeletonCounter = null;
+  
   [TextAreaAttribute]
   public string message = "default message";
   public TextMeshProUGUI tmp;
   public GameObject canvas;
+  public string skeletonId = null;
+
+  private void Start() {
+    
+  }
 
   private void OnTriggerEnter2D(Collider2D other) {
+    if (_skeletonCounter == null) {
+      _skeletonCounter = skeletonCounter.gameObject.GetComponent<SkeletonCounter>();
+    }
+
     if (other.CompareTag("Player")) {
       tmp.text = message;
       canvas.SetActive(true);
+
+      if (skeletonId != null && _skeletonCounter != null) {
+        _skeletonCounter.Add(skeletonId);
+      }
     }
   }
 
